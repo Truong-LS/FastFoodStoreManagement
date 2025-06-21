68a23ca3f07b6f9cfbdc16f0ffeddb0ed8dcec85
dungntse180117 <dungntse180117@fpt.edu.vn>
2025-06-21 18:18:47 +0700

    login

--- FastFoodStoreManagement/View/View/Login.xaml.cs
+++ FastFoodStoreManagement/View/View/Login.xaml.cs
@@ -14,9 +14,9 @@ namespace View
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Login : Window
     {
-        public MainWindow()
+        public Login()
         {
             InitializeComponent();
         }
