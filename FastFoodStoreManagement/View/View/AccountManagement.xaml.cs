86dc3fa679fb6d3753929151472b6fdb94a8569c
TsunZ <anhntse150634@fpt.edu.vn>
2025-06-23 16:28:51 +0700

    Mô tả thay đổi

---
+++ FastFoodStoreManagement/View/View/AccountManagement.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace View.View
+{
+    /// <summary>
+    /// Interaction logic for AccountManagement.xaml
+    /// </summary>
+    public partial class AccountManagement : Window
+    {
+        public AccountManagement()
+        {
+            InitializeComponent();
+        }
+    }
+}
